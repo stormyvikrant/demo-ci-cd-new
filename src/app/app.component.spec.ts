f5283e0f46bcd7d00891cc85a53b4787be27813f
vikrant prajapati <vikrant@togethring.net>
2024-07-30 10:48:52 +0530

    update testing commands

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -19,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'project1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project1');
+    expect(app.title).toEqual('project1!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project1 app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('project1!');
   });
 });
